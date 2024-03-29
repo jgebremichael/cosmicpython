31c49a40fcf02338267f79ad0b0b4f6b2079eab6
Joshua Gebremichael <jgebremichael@hotmail.com>
2024-03-29 15:57:47 -0400

    Stuck on Pg. 53

---
+++ model.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from typing import List, Optional, NewType
+from datetime import date
+
+Quantity = NewType("Quantity", int)
+Sku = NewType("Sku", str)
+Reference = NewType("Reference", str)
+
+@dataclass(frozen=True)
+class OrderLine:
+    ordered:str
+    sku:str
+    qty:int
+
+class Batch:
+    def __init__(self, ref:Reference, sku:Sku, qty:Quantity, eta:Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations = set()
+
+    def allocate(self, line:OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line:OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line:OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+def allocate(line:OrderLine, batches:List[Batch]) -> str:
+    batch = next(b for b in sorted(batches) if b.can_allocate(line))
+    batch.allocate(line)
+    return batch.reference
